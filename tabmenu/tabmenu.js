66401bce04314ea976ba17372c6e9926174deaae
정찬명 <dece24@gmail.com>
2017-03-13 18:48:54 +0900

    tabmenu.js 업데이트.

--- tabmenu/tabmenu.js
+++ tabmenu/tabmenu.js
@@ -3,12 +3,41 @@
     "use strict";
 
     $.fn.tabmenu = function () {
-        var uri = location.href.replace(location.origin, ''); // 오리진을 제외한 URL '/index.html?search#hash'
-        $(this.selector).find('a').each(function () { // .tab 메뉴 내부의 모든 a 요소를 순회.
-            var $this = $(this),
-                thisHref = $this.attr('href');
-            uri.indexOf( thisHref ) > -1 && $this.parents('li').addClass('active').end().wrapInner('<strong>').find('>strong').attr('data-href', thisHref).unwrap('a');
-            // a 요소의 href값과 uri가 일치하면 모든 조상 li 요소에 active 클래스 추가. strong요소를 넣고 thisHref값을 data 속성에 넣은 후 부모 a 요소는 제거.
+        $(this).each(function () {
+            var $tabmenu = $(this); // 선택한 탭 메뉴.
+            // 서브메뉴 있는 탭메뉴에 클래스 추가.
+            $tabmenu.find('a+ul').length && $tabmenu.addClass('_sub');
+            // 메뉴 초기화 및 하이라이팅.
+            var mark = function () {
+                // 페이지 URL 업데이트 이후 location.href 값을 취해야 한다.
+                setTimeout(function () {
+                    $tabmenu.find('a').each(function () {
+                        var $anchor = $(this);
+                        $anchor.html( $anchor.text() ).parent('li').removeClass('active');
+                        location.href.indexOf( $anchor.attr('href') ) > -1 && $anchor.wrapInner('<strong>').parentsUntil('nav', 'li').addClass('active');
+                    });
+                }, 0);
+            };
+            // 앵커 클릭으로 초기화.
+            $tabmenu.on('click', 'a', mark).find('a').trigger('click');
+
+            // 서브메뉴 롤오버 기능.
+            var submenu = function () {
+                $tabmenu
+                    .on('mouseenter', '>ul>li', function () {
+                        $(this).addClass('active').siblings('li').removeClass('active');
+                    })
+                    .on('mouseleave', mark)
+                    .on('focus', '>ul>li>a', function () {
+                        $(this).parent('li').trigger('mouseenter');
+                    })
+                    .on('focusout', function(){
+                        setTimeout(function () {
+                            $tabmenu.find(':focus').length === 0 && mark();
+                        }, 0);
+                    });
+            };
+            $tabmenu.hasClass('_sub') && submenu();
         });
         return this;
     };
