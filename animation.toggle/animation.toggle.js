5c354e9e1cc76461dddde894df3211c1aae16d51
정찬명 <dece24@sk.com>
2017-06-13 15:48:35 +0900

    animation.toggle.js 인자 처리방식 개선. 전부 객체로만 받기.

--- animation.toggle/animation.toggle.js
+++ animation.toggle/animation.toggle.js
@@ -2,58 +2,57 @@
 ;(function($){
     "use strict";
 
-    // 애니메이션을 토글하기 위한 함수.
-    $.fn.animation = function( action, option ){
-        var $this = this,
-            option = option || {},
-            animateKeepClass = option.animateKeepClass || "ani-open",
-            animateShowClass = option.animateShowClass || "ani-show",
-            animateHideClass = option.animateHideClass || "ani-hide",
-            animateTime = option.animateTime || 360;
+    // 애니메이션을 "toggle", "show", "hide" 하기 위한 함수.
+    $.fn.animation = function( obj ){
 
-        // 액션 인자를 생략했거나, "show" 또는 "hide" 아닌 경우 "toggle"으로 설정.
-        if ( !action || action !== "show" && action !== "hide" ) {
-            action = "toggle";
+        if ( typeof obj !== "object" ) { // 객체가 아닌 경우 객체로 변환.
+            obj = {};
         }
 
+        var toggleStr   = "toggle",
+            action      = obj.action     || toggleStr,
+            liveClass   = obj.liveClass  || "ani-live",
+            showClass   = obj.showClass  || "ani-show",
+            hideClass   = obj.hideClass  || "ani-hide";
+
         // 애니메이션 종료.
-        var clear = function( $this, state ){
+        var clear = function( $i, state ){
             setTimeout(function () {
-                if ( state === "show" ) {
-                    $this.removeClass(animateShowClass);
-                } else if ( state === "hide" ) {
-                    $this.removeClass(animateKeepClass + " " + animateHideClass);
+                if ( state === 1 ) {
+                    $i.removeClass(showClass);
+                } else if ( state === 2 ) {
+                    $i.removeClass(liveClass + " " + hideClass);
                 }
-            }, animateTime);
+            }, obj.time || 360 );
         };
 
         // 보여주기.
-        var show = function ($this) {
-            $this.addClass(animateKeepClass + " " + animateShowClass);
-            clear($this, "show");
+        var show = function ($i) {
+            $i.addClass(liveClass + " " + showClass);
+            clear($i, 1);
         };
 
         // 감추기.
-        var hide = function ($this) {
-            $this.addClass(animateHideClass);
-            clear($this, "hide");
+        var hide = function ($i) {
+            $i.addClass(hideClass);
+            clear($i, 2);
         };
 
         // 토글.
-        var toggle = function ($this) {
+        var toggle = function ($i) {
             // 타겟의 토글 클래스 상태에 따라 애니메이션 실행.
-            !$this.hasClass(animateKeepClass) ? show($this) : hide($this);
+            !$i.hasClass(liveClass) ? show($i) : hide($i);
         };
 
         // action 인자에 따라 필요한 기능 실행.
         this.each(function () {
-            var $this = $(this);
-            if ( action === "toggle" ) {
-                toggle($this);
+            var $i = $(this);
+            if ( action === toggleStr ) {
+                toggle($i);
             } else if ( action === "show" ) {
-                show($this);
+                show($i);
             } else if ( action === "hide" ) {
-                hide($this);
+                hide($i);
             }
         });
 
